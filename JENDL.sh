27a4c3d33af1d23706eb3d1a097bda37655944c6
Vivian SALINO <firstname.surname@irsn.fr>
2023-09-21 06:20:57 +0200

    Correct a typo (JENDL-4.3 -> 3.3)

--- JENDL.sh
+++ JENDL.sh
@@ -10,7 +10,7 @@ do
         url="https://wwwndc.jaea.go.jp/cgi-bin/evlret?Z=&A=&Selection=1&Lib=39"
     elif [[ "$version" == "3.2" ]]; then
         url="https://wwwndc.jaea.go.jp/cgi-bin/evlret?Z=&A=&Selection=1&Lib=14"
-    elif [[ "$version" == "4.3" ]]; then
+    elif [[ "$version" == "3.3" ]]; then
         url="https://wwwndc.jaea.go.jp/cgi-bin/evlret?Z=&A=&Selection=1&Lib=0&Lib=17"
     elif [[ "$version" == "4.0" ]]; then
         url="https://wwwndc.jaea.go.jp/cgi-bin/evlret?Z=&A=&Selection=1&Lib=31"
